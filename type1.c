3e7ded0ba1f02af09529f60cea94edc499a826eb
dopler <dopleref@gmail.com>
2017-02-07 19:56:51 +0500

    саша добавил файл в котором он будет писать C-код для решения задач

---
+++ type1.c
@@ -0,0 +1,45 @@
+#include <>
+
+struct data {
+    int DoC;
+    char *formyla;
+    int prividenie;
+    int M;
+    int Mvesh;
+
+};
+
+
+enum subs {air = 29,O2 = 32,H2 = 2};
+
+int main()
+{
+
+
+
+}
+
+char *type1(int PH,char *Substation,double density)
+{
+    struct data adata = {};
+
+   char *vesh1 = "Кислород";
+   char *vesh2 = "Водород";
+   char *vesh3 = "Воздух";
+    if(strcmp(Substation,vesh1)){
+
+    
+    }
+            
+
+
+}
+
+int algebra()
+{
+
+
+
+
+
+}
